239f283dd850fd8f35bf96426f66bae5610098ee
Alan Turing <freemasl.sf@gmail.com>
2019-05-28 15:30:02 -0600

    Complete sphinx test

--- exercises/sphinx.js
+++ exercises/sphinx.js
@@ -1,6 +1,7 @@
 class Sphinx {
   constructor() {
     this.riddles = [];
+    this.heroesEaten = 0;
   }
 
   collectRiddle(riddle) {
@@ -11,8 +12,16 @@ class Sphinx {
   }
 
   attemptAnswer(answer) {
-  console.log(this.riddles.answer)
-    this.riddles.pop();
+    for (var i = 0; i < this.riddles.length; i++) {
+      if (answer === this.riddles[i].answer) {
+        this.riddles.splice(i, 1);
+      }
+      if (this.riddles.length === 0) {
+        return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answer}"???`;
+      }
+    }
+    this.heroesEaten++;
+    return 'That wasn\'t that hard, I bet you don\'t get the next one';
   }
 }
 
